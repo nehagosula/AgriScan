23559f585dfb8f4ed7a9d2842ea6e38992803505
Neha gosula <nehareddy860@gmail.com>
2026-04-27 00:02:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def api_predict():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
     app.run(host="0.0.0.0", port=port)
     #app.run(host="0.0.0.0", port=port, debug=debug)
